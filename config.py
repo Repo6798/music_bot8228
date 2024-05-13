83f272ce2f5fb97024024dce1dcac930b26b95d2
Bhola116 <169025421+Bhola116@users.noreply.github.com>
2024-05-13 15:05:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/theind_coders")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/the_ind_coders")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/crunchyrollanimeinhindidubb")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Team_Shadow_Group")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
